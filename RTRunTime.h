c418c29e0d6f8961e0502a6db0e706220626b1df
chensheng12330 <chensheng12330@gmail.com>
2014-05-15 23:13:01 +0800

    add all files

---
+++ RTRunTime.h
@@ -0,0 +1,20 @@
+//
+//  RTRunTime.h
+//  UMSocial_Sdk_Demo
+//
+//  Created by sherwin on 14-5-15.
+//  Copyright (c) 2014年 yeahugo. All rights reserved.
+//
+
+#ifndef UMSocial_Sdk_Demo_RTRunTime_h
+#define UMSocial_Sdk_Demo_RTRunTime_h
+
+#import "RTIvar.h"      //变量
+#import "RTMethod.h"    //方法
+#import "RTProperty.h"  //属性设置
+#import "RTProtocol.h"  //代理
+#import "MARTNSObject.h"//
+#import "RTUnregisteredClass.h"
+
+
+#endif

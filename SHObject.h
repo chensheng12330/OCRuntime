4987e6e1b72803096523a52c89fe3adc4bb7f889
chensheng12330 <chensheng12330@gmail.com>
2014-05-16 18:06:35 +0800

    test class

---
+++ SHObject.h
@@ -0,0 +1,13 @@
+//
+//  SHObject.h
+//  UMSocial_Sdk_Demo
+//
+//  Created by sherwin on 14-5-16.
+//  Copyright (c) 2014年 yeahugo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHObject : NSObject
+
+@end
---
+++ SHObject.m
@@ -0,0 +1,13 @@
+//
+//  SHObject.m
+//  UMSocial_Sdk_Demo
+//
+//  Created by sherwin on 14-5-16.
+//  Copyright (c) 2014年 yeahugo. All rights reserved.
+//
+
+#import "SHObject.h"
+
+@implementation SHObject
+
+@end
